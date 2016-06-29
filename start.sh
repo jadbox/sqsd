3734ee97cb7539dcea4ed56b6f7dfdebf1b39087
Jonathan Dunlap <jdunlap@outlook.com>
2016-06-29 10:39:06 -0700

    added then back

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 echo "sqsd starting"
 if [ -n "$SQSD_WORKER_PORT_80_TCP_ADDR" ]; 
-	export SQSD_WORKER_HTTP_URL=http://${SQSD_WORKER_PORT_80_TCP_ADDR}:8800/sqs; 
+	then export SQSD_WORKER_HTTP_URL=http://${SQSD_WORKER_PORT_80_TCP_ADDR}:8800/sqs; 
 fi
 
-if [ -n "$SQSD_WORKER_PORT_8800_TCP_ADDR" ];
-	export SQSD_WORKER_HTTP_URL=http://${SQSD_WORKER_PORT_8800_TCP_ADDR}:8800/sqs; 
+if [ -n "$SQSD_WORKER_PORT_8800_TCP_ADDR" ]; 
+	then export SQSD_WORKER_HTTP_URL=http://${SQSD_WORKER_PORT_8800_TCP_ADDR}:8800/sqs; 
 fi
 
 export SQSD_WORKER_HTTP_URL=${SQSD_WORKER_HTTP_URL}
