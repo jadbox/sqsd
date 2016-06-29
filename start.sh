1b9e8344322b28bc41acb78836a7a0092feb4d0c
Jonathan Dunlap <jdunlap@outlook.com>
2016-06-29 10:29:22 -0700

    format start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 echo "sqsd starting"
-if [ -n "$SQSD_WORKER_PORT_80_TCP_ADDR" ]; export SQSD_WORKER_HTTP_URL=http://${SQSD_WORKER_PORT_80_TCP_ADDR}:8800/sqs; fi
-if [ -n "$SQSD_WORKER_PORT_8800_TCP_ADDR" ]; then export SQSD_WORKER_HTTP_URL=http://${SQSD_WORKER_PORT_8800_TCP_ADDR}:8800/sqs; fi
+if [ -n "$SQSD_WORKER_PORT_80_TCP_ADDR" ]; 
+	export SQSD_WORKER_HTTP_URL=http://${SQSD_WORKER_PORT_80_TCP_ADDR}:8800/sqs; 
+fi
+
+if [ -n "$SQSD_WORKER_PORT_8800_TCP_ADDR" ]; 
+	then export SQSD_WORKER_HTTP_URL=http://${SQSD_WORKER_PORT_8800_TCP_ADDR}:8800/sqs; 
+fi
+
 export SQSD_WORKER_HTTP_URL=${SQSD_WORKER_HTTP_URL}
 echo "worker_url ${SQSD_WORKER_HTTP_URL}"
 echo "npm start"
